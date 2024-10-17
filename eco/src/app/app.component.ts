a606208e103aa3f41cf07868f215786eeff7a38c
Nguyen Van Vuong <hayasilver123@gmail.com>
2024-10-17 22:53:44 +0700

    handle login component

--- eco/src/app/app.component.ts
+++ eco/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { PrimeNGConfig } from 'primeng/api';
+import { AuthStore } from './shared/stores/auth.store';
+import { LOGIN_URL } from './shared/constants/url.const';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +14,15 @@ export class AppComponent {
   menuMode = 'static';
   constructor(
     private primeNgConfig : PrimeNGConfig,
-    private router: Router
+    private router: Router,
+    private authStore: AuthStore
     ){
   }
   ngOnInit(): void {
     this.primeNgConfig.ripple = true;
     document.documentElement.style.fontSize = '14px';
-  };
+    if(this.authStore.isAuthenticated()){
+      this.router.navigate([LOGIN_URL]);
+    }
+  }
 }
--- eco/src/app/auth/login/login.component.ts
+++ eco/src/app/auth/login/login.component.ts
@@ -1,23 +1,87 @@
 import { Component } from '@angular/core';
 import { LayoutService } from '../../layout/service/app.layout.service';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { LoginModel } from '../../shared/models/auth.dto';
+import { Router } from '@angular/router';
+import { JwtService } from '../../shared/services/jwt.service';
+import { Subject, takeUntil } from 'rxjs';
+import { HOME_URL } from '../../shared/constants/url.const';
+import { AuthStore } from '../../shared/stores/auth.store';
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styles: [`
-        :host ::ng-deep .pi-eye,
-        :host ::ng-deep .pi-eye-slash {
-            transform:scale(1.6);
-            margin-right: 1rem;
-            color: var(--primary-color) !important;
-        }
-    `]
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styles: [
+    `
+      :host ::ng-deep .pi-eye,
+      :host ::ng-deep .pi-eye-slash {
+        transform: scale(1.6);
+        margin-right: 1rem;
+        color: var(--primary-color) !important;
+      }
+    `,
+  ],
 })
 export class LoginComponent {
+  valCheck: string[] = ['remember'];
 
-    valCheck: string[] = ['remember'];
+  password!: string;
 
-    password!: string;
+  loginForm: FormGroup;
 
-    constructor(public layoutService: LayoutService) { }
+  public blockedPanel: boolean = false;
+
+  private ngUnsubscribe = new Subject<void>();
+
+  constructor(
+    public layoutService: LayoutService,
+    private fb: FormBuilder,
+    private authStore: AuthStore,
+    private router: Router,
+    private jwtService: JwtService,
+) {
+    this.loginForm = this.fb.group({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+    });
+  }
+
+  login() {
+    this.toggleBlockUI(true);
+    var request: LoginModel = {
+      username: this.loginForm.controls['username'].value,
+      password: this.loginForm.controls['password'].value,
+    };
+  
+    // Gọi AuthStore thay vì AuthService
+    this.authStore
+      .login(request.username, request.password)
+      .then(() => {
+        this.toggleBlockUI(false);
+        this.router.navigate([HOME_URL]);
+      })
+      .catch((error) => {
+        this.toggleBlockUI(false);
+      });
+  }
+  
+  private toggleBlockUI(enabled: boolean) {
+    if (enabled == true) {
+      this.blockedPanel = true;
+    } else {
+      setTimeout(() => {
+        this.blockedPanel = false;
+      }, 1000);
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
 }
