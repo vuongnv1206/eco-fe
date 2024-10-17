662697fd32a1d4734d5158d6e003ee9d389dffd1
Nguyen Van Vuong <hayasilver123@gmail.com>
2024-10-17 23:20:11 +0700

    add top bar module

--- eco/src/app/app.module.ts
+++ eco/src/app/app.module.ts
@@ -4,6 +4,11 @@ import { AppComponent } from './app.component';
 import { AppLayoutModule } from './layout/app.layout.module';
 import { AppRoutingModule } from './app.routes';
 import { HttpClient } from '@angular/common/http';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ToastModule } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ImageModule } from 'primeng/image';
 @NgModule({
   declarations: [	
     AppComponent,
@@ -12,6 +17,12 @@ import { HttpClient } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     AppLayoutModule,
+    ConfirmDialogModule,
+    ToastModule,
+    ButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ImageModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- eco/src/app/layout/app.topbar.component.ts
+++ eco/src/app/layout/app.topbar.component.ts
@@ -1,6 +1,9 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { LayoutService } from "./service/app.layout.service";
+import { Router } from '@angular/router';
+import { AuthService } from '../shared/services/auth.services';
+import { LOGIN_URL } from '../shared/constants/url.const';
 
 @Component({
     selector: 'app-topbar',
@@ -8,7 +11,7 @@ import { LayoutService } from "./service/app.layout.service";
 })
 export class AppTopBarComponent {
 
-    items!: MenuItem[];
+    userMenuItems!: MenuItem[];
 
     @ViewChild('menubutton') menuButton!: ElementRef;
 
@@ -16,5 +19,45 @@ export class AppTopBarComponent {
 
     @ViewChild('topbarmenu') menu!: ElementRef;
 
-    constructor(public layoutService: LayoutService) { }
+    constructor(public layoutService: LayoutService,
+        private authService : AuthService,
+        private router: Router
+        ) { }
+  
+      ngOnInit(): void {
+        this.userMenuItems = [
+          {
+            label: 'File',
+            items: [{
+                    label: 'Personal Information',
+                    icon: 'pi pi-id-card',
+                    routerLink: '/profile',
+                    items: [
+                        {label: 'Project'},
+                        {label: 'Other'},
+                    ]
+                },
+                {label: 'Open'},
+                {label: 'Quit'}
+            ]
+        },
+        {
+            label: 'Change Password',
+            icon: 'pi pi-key',
+            routerLink: '/change-password',
+            items: [
+                {label: 'Delete', icon: 'pi pi-fw pi-trash'},
+                {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
+            ]
+        },
+        {
+            label: 'Logout',
+            icon: 'pi pi-sign-out',
+            command: event => {
+              this.authService.signOut();
+              this.router.navigate([LOGIN_URL]);
+            }
+        }
+        ];
+      }
 }
